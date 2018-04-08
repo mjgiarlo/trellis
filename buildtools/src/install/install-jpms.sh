a24ffede6eebeeb3e084218384e5f1e7651e8b6a
Christopher Hanna Johnson <chjohnson39@gmail.com>
2018-04-08 12:07:56 +0200

    experimental gradle build script with code coverage

--- buildtools/src/install/install-jpms.sh
+++ buildtools/src/install/install-jpms.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-
-./gradlew trellis-vocabulary:build
-./gradlew trellis-api:build
-./gradlew trellis-id:build
-./gradlew trellis-io:build
-./gradlew trellis-namespaces:build
-./gradlew trellis-http:build
-./gradlew trellis-file:build
-./gradlew trellis-jms:build
-./gradlew trellis-kafka:build
-./gradlew trellis-test:build
-./gradlew trellis-triplestore:build
-./gradlew trellis-webac:build
-./gradlew build
+./gradlew --version
+./gradlew --scan --stacktrace --warning-mode=all trellis-vocabulary:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-api:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-id:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-io:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-namespaces:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-http:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-file:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-amqp:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-jms:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-kafka:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-test:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-triplestore:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-webac:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-agent:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-audit:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-app:build
+./gradlew --scan --stacktrace --warning-mode=all trellis-osgi:build
