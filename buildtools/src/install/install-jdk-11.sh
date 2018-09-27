880629042e7d0ec83798b2ce27ab11739a5b7c32
Christopher Johnson <chjohnson39@gmail.com>
2018-09-27 16:00:01 +0200

    adds oraclelicense accept cookie for jdk11 download script adds tamaya-core, javax.annotation as testDependencies in trellis-webac and trellis-app

--- buildtools/src/install/install-jdk-11.sh
+++ buildtools/src/install/install-jdk-11.sh
@@ -6,7 +6,7 @@ JDK_FEATURE=11
 JDK_ARCHIVE=jdk-${JDK_FEATURE}_linux-x64_bin.tar.gz
 
 cd ~
-wget http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_linux-x64_bin.tar.gz
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
